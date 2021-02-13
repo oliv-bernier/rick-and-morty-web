6db976e7ee9af6db3b3863093ecb1c5692e11d28
oliv-bernier <olivbernier@gmail.com>
2021-02-13 23:15:55 +0100

    details responsive

--- my-app/src/components/Details/index.js
+++ my-app/src/components/Details/index.js
@@ -15,37 +15,37 @@ const CharacterDetails = ({ oneCharacter, closeDetails }) => {
     return (
         <section className="details">
             <div className="details__global">
-            <img className="details__global-image" src={oneCharacter.image} alt=""></img>
-            <ul className="details__global-list">
-                {/* {details.map((detail) => {
-                    <li>id: {detail.id}</li>
-                })} */}
-                <li className="details__global-list-item">
-                        id: {oneCharacter.id}
-                </li>
-                <li className="details__global-list-item">
-                        name: {oneCharacter.name}
-                </li>
-                <li className="details__global-list-item">
-                        status: {oneCharacter.status}
-                </li>
-                <li className="details__global-list-item">
-                        species: {oneCharacter.species}
-                </li>
-                <li className="details__global-list-item">
-                        gender: {oneCharacter.gender}
-                </li>
-                <li className="details__global-list-item">
-                        origin: {oneCharacter.origin.name}
-                </li>
-                <li className="details__global-list-item">
-                        location: {oneCharacter.location.name}
-                </li>
-                {/* <li className="details__global-list-item">
-                        episode: 
-                </li> */}
-            </ul>
-            <button className="details__global-button" onClick={handleClose}>x</button>
+                <img className="details__global-image" src={oneCharacter.image} alt=""></img>
+                <ul className="details__global-list">
+                    {/* {details.map((detail) => {
+                        <li>id: {detail.id}</li>
+                    })} */}
+                    <li className="details__global-list-item">
+                            #{oneCharacter.id}
+                    </li>
+                    <li className="details__global-list-item">
+                            name: {oneCharacter.name}
+                    </li>
+                    <li className="details__global-list-item">
+                            status: {oneCharacter.status}
+                    </li>
+                    <li className="details__global-list-item">
+                            species: {oneCharacter.species}
+                    </li>
+                    <li className="details__global-list-item">
+                            gender: {oneCharacter.gender}
+                    </li>
+                    <li className="details__global-list-item">
+                            origin: {oneCharacter.origin.name}
+                    </li>
+                    <li className="details__global-list-item">
+                            location: {oneCharacter.location.name}
+                    </li>
+                    {/* <li className="details__global-list-item">
+                            episode: 
+                    </li> */}
+                </ul>
+                <button className="details__global-button" onClick={handleClose}>x</button>
             </div>
         </section>
     );
